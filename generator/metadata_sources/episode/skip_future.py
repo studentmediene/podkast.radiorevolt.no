4edf9a8a3392e947072c090e1a17fdcc849206b0
Thorben Dahl <thorben@sjostrom.no>
2016-04-05 14:54:40 +0200

    Fix SkipFutureEpisodes not using timezone-aware now-datetime.

--- generator/metadata_sources/episode/skip_future.py
+++ generator/metadata_sources/episode/skip_future.py
@@ -1,5 +1,7 @@
 from .. import SkipEpisode, EpisodeMetadataSource
 from datetime import datetime
+from pytz import timezone
+import pytz
 
 
 class SkipFutureEpisodes(EpisodeMetadataSource):
@@ -7,7 +9,7 @@ class SkipFutureEpisodes(EpisodeMetadataSource):
         EpisodeMetadataSource.__init__(self)
 
     def accepts(self, episode) -> bool:
-        return episode.date > datetime.utcnow()
+        return episode.date > datetime.now(pytz.utc)
 
     def populate(self, episode) -> None:
         raise SkipEpisode
