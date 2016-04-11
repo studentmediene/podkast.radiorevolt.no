87d661bdf6c29d0cc68be21b30a77e07fc7678a5
Thorben Dahl <thorben@sjostrom.no>
2016-04-11 08:11:10 +0200

    Remove support for running in parallels
    Each parallel had to fetch its own list of episodes, metadata and
    so on. Therefore, it turned out to be more efficient to do everything
    in one single process. (Parallels could only compete when you spawned
    20 forks, but then you always had processes that got delayed because
    the metadata host would take some time to respond.)
    
    I also believe this is for the best, considering load and stress
    for the Radio REST API.

--- batch_generate_feed.py
+++ batch_generate_feed.py
@@ -1,13 +1,7 @@
 import argparse
 import os.path
 from generator.generate_feed import PodcastFeedGenerator
-import multiprocessing
-from generator import NoEpisodesError, NoSuchShowError
 import tempfile
-from time import sleep
-
-# Control how many processes can run at any given time
-parallels_semaphore = None
 
 def save_feed_to_file(feed, target_file):
     with tempfile.NamedTemporaryFile(delete=False) as fp:
@@ -16,26 +10,8 @@ def save_feed_to_file(feed, target_file):
     os.replace(tempname, target_file)
 
 
-def write_feed(show_id, target_file, pretty_xml, durations):
-    podcasts = PodcastFeedGenerator(pretty_xml, durations, True)
-    try:
-        feed = podcasts.generate_feed(show_id, False)
-        save_feed_to_file(feed, target_file)
-    except (NoSuchShowError, NoEpisodesError):
-        # TODO: Log this
-        pass
-    finally:
-        # Make our spot free
-        parallels_semaphore.release()
-
-
 def parse_cli_arguments():
     parser = argparse.ArgumentParser(description="Write feeds for multiple podcasts")
-
-    parser.add_argument("--parallels", type=int, default=1,
-                        help="NOT IMPLEMENTED. Number of feeds that can be generated at the same time. Default: 1."
-                        "WARNING: Limitations on downloads and memory usage are applied "
-                        "PER PARALLEL, so please be conservative or risk running out of memory!")
     parser.add_argument("--create-directory", "-d", action="store_true",
                         help="Create target_dir if it doesn't exist already.")
     parser.add_argument("target_dir", type=os.path.abspath, default=".",
@@ -67,7 +43,6 @@ def main():
     quiet = args.quiet
     pretty = args.pretty
     create_dir = args.create_directory
-    parallels = args.parallels
 
     all_shows = PodcastFeedGenerator(pretty, calculate_durations, quiet).show_source.shows
     all_shows_set = set(all_shows.keys())
@@ -91,9 +66,6 @@ def main():
         parser.error("naming_scheme must contain %t, %i, %T or a combination in order to generate unique filenames for "
                      "each show.")
 
-    if parallels <= 0:
-        parser.error("Argument to --parallels must be positive, not {value}.".format(value=parallels))
-
     chosen_shows_dict = {show_id: all_shows[show_id].title for show_id in chosen_shows}
 
     filenames = dict()
@@ -109,47 +81,14 @@ def main():
             filename = filename.replace("\\", "_")
         filenames[show_id] = os.path.join(target_dir, os.path.normcase(filename))
 
-    if parallels == 1:
-        # Run everything in one single process, reusing resources
-        g = PodcastFeedGenerator(pretty, calculate_durations, quiet)
-        feeds = g.generate_all_feeds_sequence()
-        # Save to files
-        if not quiet:
-            print("Writing feeds to files...")
-        for show_id, feed in feeds.items():
-            save_feed_to_file(feed, filenames[show_id])
-    else:
-        # Open one process per feed
-        processes = list()
-        for show_id, show_title in chosen_shows_dict.items():
-            processes.append(multiprocessing.Process(
-                target=write_feed,
-                args=(show_id, filenames[show_id], pretty, calculate_durations),
-                name=show_title
-            ))
-
-        global parallels_semaphore
-        parallels_semaphore = multiprocessing.BoundedSemaphore(parallels)
-        num_feeds = len(processes)
-        done_feeds = - parallels
-
-        for i, process in enumerate(processes):
-            parallels_semaphore.acquire()
-            done_feeds += 1
-            print_progress(max(done_feeds, 0), num_feeds, quiet)
-            process.start()
-
-        while done_feeds < num_feeds:
-            new_done_feeds = num_feeds - len(multiprocessing.active_children())
-            if new_done_feeds != done_feeds:
-                done_feeds = new_done_feeds
-                print_progress(done_feeds, num_feeds, quiet)
-            sleep(0.1)
-
-
-def print_progress(done, all, quiet):
+    # Run everything in one single process, reusing resources
+    g = PodcastFeedGenerator(pretty, calculate_durations, quiet)
+    feeds = g.generate_all_feeds_sequence()
+    # Save to files
     if not quiet:
-        print("({done:03}/{all:03})".format(done=done, all=all), end="\r")
+        print("Writing feeds to files...")
+    for show_id, feed in feeds.items():
+        save_feed_to_file(feed, filenames[show_id])
 
 
 if __name__ == '__main__':
