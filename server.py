0f8c99d4e146602596247752aa9758be35d1c0f4
Thorben Dahl <thorben@sjostrom.no>
2016-04-26 14:50:44 +0200

    Fix error in get_api_help in c2f673d8066b

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
 import webserver.feed_server
 import argparse
 
+
 def parse_cli_arguments():
     parser = argparse.ArgumentParser(description="Run a server suitable for development purposes.")
+    parser.add_argument("--debug", "-d", action="store_true",
+                        help="Activate debugging, overriding the option in webserver/settings.py "
+                             "(you shouldn't use this script in production, but especially not with"
+                             " this option!! You might reveal secret information to others.)")
     parser.add_argument("host", nargs="?", default="127.0.0.1", help="Accept connections for this host. "
                         "Set to 0.0.0.0 to enable connections from anywhere (not safe!). "
                         "Defaults to 127.0.0.1, which means only connections from this computer.")
@@ -12,4 +17,7 @@ def parse_cli_arguments():
 if __name__ == '__main__':
     parser, args = parse_cli_arguments()
     host = args.host
+
+    if args.debug:
+        webserver.feed_server.app.debug = True
     webserver.feed_server.app.run(host=host)
