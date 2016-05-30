2099ef2c86fa6626499793a1692f1d9466ecbdae
Thorben Dahl <thorben@sjostrom.no>
2016-05-30 23:44:30 +0200

    Add default configuration for logging inside generator

--- generator/__init__.py
+++ generator/__init__.py
@@ -2,3 +2,9 @@ from .no_such_show_error import NoSuchShowError
 from .no_episodes_error import NoEpisodesError
 from .episode import Episode
 from .show import Show
+
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+from logging import NullHandler
+_logger = logging.getLogger(__name__)
+_logger.addHandler(NullHandler())
