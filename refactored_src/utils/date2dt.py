0da05298b8561536bdb0e20c9f7ebaab65bced39
Thorben Werner Sjøstrøm Dahl <thorben@sjostrom.no>
2017-11-09 22:35:41 +0100

    Propagate None values sent to date2dt

--- refactored_src/utils/date2dt.py
+++ refactored_src/utils/date2dt.py
@@ -3,5 +3,7 @@ from datetime import time, timezone, datetime
 
 def date2dt(date):
     """Convert date into timezone-aware datetime (with time=UTC midnight)."""
+    if not date:
+        return date
     midnight = time(tzinfo=timezone.utc)
     return datetime.combine(date, midnight)
