ffe78e2fd6f69bbc54cc2c453d1e3ce842f1a586
Thorben Werner Sjøstrøm Dahl <thorben@sjostrom.no>
2017-11-09 21:14:52 +0100

    Implement processor for Show which sets values forcefully

---
+++ refactored_src/show_processors/force_values.py
@@ -0,0 +1,22 @@
+from .set_defaults import SetDefaults
+
+
+class ForceValues(SetDefaults):
+    """
+    Override values set so far with values from this processor's settings.
+
+    The accepted settings are the same as for SetDefaults. Leave a setting
+    out in order to not override it, or set it to a falsy value (None for
+    explicit).
+    """
+    @staticmethod
+    def _set_if_false(show, attribute, default_value):
+        # Replace the if-false check with True, so we always set
+        if default_value:
+            setattr(show, attribute, default_value)
+
+    @staticmethod
+    def _set_if_none(show, attribute, default_value):
+        # Replace the if-none check with True, so we always set
+        if default_value is not None:
+            setattr(show, attribute, default_value)
--- refactored_src/show_processors/set_defaults.py
+++ refactored_src/show_processors/set_defaults.py
@@ -7,18 +7,48 @@ __all__ = ["SetDefaults"]
 
 
 class SetDefaults(ShowProcessor):
+    """
+    Populate fields in the show which aren't populated yet, using the settings.
+
+    Settings:
+        description: Podcast description
+        category:
+          category: Main Itunes category
+          subcategory: Sub Itunes category
+        language: Podcast language
+        website: Website podcast links to as its website
+        authors:
+          - name: Name of author
+          - email: Email of author
+          ...
+        web_master:
+          name: Name of web master
+          email: Email of web master
+        explicit: true/false, whether the podcast should not be played for kids
+        owner:
+          name: Name of owner
+          email: Email of owner
+    """
     def accepts(self, show) -> bool:
         return super().accepts(show)
 
     def populate(self, show) -> None:
-        self._set_if_false(show, "description", self.settings['description'])
-        self._set_if_false(show, "category", Category(**self.settings['category']))
-        self._set_if_false(show, "language", self.settings['language'])
-        self._set_if_false(show, "website", self.settings['website'])
-        self._set_if_false(show, "authors", [Person(**author) for author in self.settings['authors']])
-        self._set_if_false(show, "web_master", Person(**self.settings['web_master']))
-        self._set_if_none(show, "explicit", self.settings['explicit'])
-        self._set_if_false(show, "owner", Person(**self.settings['owner']))
+        self._set_if_false(show, "description", self.settings.get('description'))
+
+        default_category = Category(**self.settings['category']) if self.settings.get('category') else None
+        self._set_if_false(show, "category", default_category)
+
+        self._set_if_false(show, "language", self.settings.get('language'))
+        self._set_if_false(show, "website", self.settings.get('website'))
+        self._set_if_false(show, "authors", [Person(**author) for author in self.settings['authors']] if self.settings.get('authors') else [])
+
+        default_webmaster = Person(**self.settings['web_master']) if self.settings.get('web_master') else None
+        self._set_if_false(show, "web_master", default_webmaster)
+
+        self._set_if_none(show, "explicit", self.settings.get('explicit'))
+
+        default_owner = Person(**self.settings['owner']) if self.settings.get('owner') else None
+        self._set_if_false(show, "owner", default_owner)
 
         if show.owner:
             self._set_if_false(show, "copyright", show.owner.name)
