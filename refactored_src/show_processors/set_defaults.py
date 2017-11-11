f94786809675f4c63a26cad3580abf89b7d7da55
Thorben Werner Sjøstrøm Dahl <thorben@sjostrom.no>
2017-11-11 13:59:59 +0100

    Set default image for shows

--- refactored_src/show_processors/set_defaults.py
+++ refactored_src/show_processors/set_defaults.py
@@ -33,25 +33,27 @@ class SetDefaults(ShowProcessor):
         return super().accepts(show)
 
     def populate(self, show) -> None:
-        self._set_if_false(show, "description", self.settings.get('description'))
+        self._set_if_false(show, 'description', self.settings.get('description'))
 
         default_category = Category(**self.settings['category']) if self.settings.get('category') else None
-        self._set_if_false(show, "category", default_category)
+        self._set_if_false(show, 'category', default_category)
 
-        self._set_if_false(show, "language", self.settings.get('language'))
-        self._set_if_false(show, "website", self.settings.get('website'))
-        self._set_if_false(show, "authors", [Person(**author) for author in self.settings['authors']] if self.settings.get('authors') else [])
+        self._set_if_false(show, 'language', self.settings.get('language'))
+        self._set_if_false(show, 'website', self.settings.get('website'))
+        self._set_if_false(show, 'authors', [Person(**author) for author in self.settings['authors']] if self.settings.get('authors') else [])
 
         default_webmaster = Person(**self.settings['web_master']) if self.settings.get('web_master') else None
-        self._set_if_false(show, "web_master", default_webmaster)
+        self._set_if_false(show, 'web_master', default_webmaster)
 
-        self._set_if_none(show, "explicit", self.settings.get('explicit'))
+        self._set_if_none(show, 'explicit', self.settings.get('explicit'))
 
         default_owner = Person(**self.settings['owner']) if self.settings.get('owner') else None
-        self._set_if_false(show, "owner", default_owner)
+        self._set_if_false(show, 'owner', default_owner)
 
         if show.owner:
-            self._set_if_false(show, "copyright", show.owner.name)
+            self._set_if_false(show, 'copyright', show.owner.name)
+
+        self._set_if_false(show, 'image', self.settings.get('image'))
 
     @staticmethod
     def _set_if_false(show, attribute, default_value):
