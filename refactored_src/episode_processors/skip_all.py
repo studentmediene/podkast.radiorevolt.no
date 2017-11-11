d15d66f2db9edf0d2d64c69587933a15d015526e
Thorben Werner Sjøstrøm Dahl <thorben@sjostrom.no>
2017-11-11 13:48:25 +0100

    Implement SkipAll processor for episodes
    This can be used to limit which episodes are available for
    Spotify.

---
+++ refactored_src/episode_processors/skip_all.py
@@ -0,0 +1,9 @@
+from . import EpisodeProcessor, SkipEpisode
+
+
+class SkipAll(EpisodeProcessor):
+    def accepts(self, episode) -> bool:
+        return super().accepts(episode)
+
+    def populate(self, episode) -> None:
+        raise SkipEpisode("Skipped because it was accepted by SkipAll")
